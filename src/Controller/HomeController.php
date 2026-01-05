78ec508936ec9574e02fac9f700f8ec773fcef9e
Valerie Lavoisier <val.lavoisier3@gmail.com>
2026-01-05 18:19:13 +0100

    création de la page d'accueil, factorisation menu

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(ProjectRepository $projectRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'projects' => $projectRepository->findAll(),
+        ]);
+    }
+}
